31ea54b2dcddf609dc58ebbd64bcc6fe90fc7a9c
Karolis Rusenas <karolis@rusenas.dev>
2023-03-24 21:49:43 +0000

    SA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
@@ -7,6 +8,15 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js"  />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
       <Analytics />
     </SessionProvider>
   );
